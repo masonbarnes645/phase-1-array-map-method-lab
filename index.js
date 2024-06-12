fa1b4d74f5417b2fcb576921c8958dd2d89d1041
Mason Barnes <barnesmason980@gmail.com>
2024-06-12 15:37:25 -0700

    Lab Complete

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+//const titleCased = () => {
+  
+//}
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +16,24 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+//! iterate through each string in the array, capitalizing the first letter of each word
+// make function using map to iterate through each string
+// make function that iterates through each word and makes the first letter capital
+
+
+
+
+
+
+
+function titleCased(){
+  return tutorials.map(function(string) {
+    return string.split(" ").map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  });
 }
+
+//titleCased
